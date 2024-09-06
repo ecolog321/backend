791234bdc84a9613cc066312bb0b7ad7c81c4a35
ecolog321 <ecakrhipov@gmail.com>
2024-09-06 13:23:05 +0800

    fix names

--- src/app.js
+++ src/app.js
@@ -8,12 +8,29 @@ dotenv.config();
 
 const userRoutes = require('./routes/users');
 const bookRoutes = require('./routes/books');
-const { errorHandler } = require('./middleware/errorMiddleware');
-const { loggerMiddleware } = require('./middleware/loggerMiddleware');
+const { errorHandler } = require('./middlewares/error');
+const { loggerMiddleware } = require('./middlewares/logger');
 
 const app = express();
 
+const {
+    API_URL='127.0.0.1',
+    PORT=3005,
+    MONGO_URL='mongodb://localhost:27017/mydb'
+} = process.env;
 
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://127.0.0.1:${PORT}`);
+    });
+})
+.catch((error) => {
+    console.error('Database connection error:', error);
+});
 
 // Middleware
 app.use(express.json());
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,10 +3,10 @@ const userController = require('../controllers/users');
 
 const router = express.Router();
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
+router.get('/', userController.getUsers);
+router.get('/:id', userController.getUser);
 router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
+router.put('/:id', userController.editUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
